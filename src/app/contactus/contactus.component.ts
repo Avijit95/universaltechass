7888c821d406e813595b3ac73105c23d46a3cdcc
influxiq1 <himadribar.influxiq@gmail.com>
2019-09-02 14:51:53 +0530

    himadri

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormBuilder, Validators} from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import {FormControl, FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 declare var $: any;
 var arr:any;
 
@@ -10,38 +10,69 @@ var arr:any;
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
-  public myform: any;
+  public myform : FormGroup;
   public data: any;
+  public stateslist: any;
 
-  constructor(public formbuilder: FormBuilder, public _http: HttpClient) {
+  constructor(public formbuilder: FormBuilder, public http: HttpClient) {
 
+    this.getState();
 
     this.myform = this.formbuilder.group({
 
-      fname: ['', Validators.required],
-      lname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
       address: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
       zipcode: ['', Validators.required],
       country: ['', Validators.required],
-      pnumber: ['', Validators.compose([Validators.required, Validators.pattern(/[0-9\+\-\ ]/)])],
+      telephone: ['', Validators.compose([Validators.required, Validators.pattern(/[0-9\+\-\ ]/)])],
       email: ['', Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
-      comment: ['', Validators.required],
-
-
-
+      message: ['', Validators.required],
     })
   }
 
   ngOnInit() {
   }
+  getState() {
+    const httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+        })
+    };
+    var result = this.http.get('assets/data/state.json').subscribe(res =>{
+      this.stateslist = res;
+      console.log('stateslist');
+      console.log(this.stateslist);
+
+    });
+    return result;
+}
 
   doSubmit(){
     console.log('ok');
     this.data = this.myform.value;
     for (let i in this.myform.controls) {
       this.myform.controls[i].markAsTouched();
+    }
+    if(this.myform.valid) {
+      let link: any;
+      link = 'http://192.169.196.208:7061/utacontactmail';
+      let data: any = this.myform.value;
+      this.http.post(link, data).subscribe(response =>{
+        let result: any;
+        result = response;
+        if(result.status == 'success'){
+          console.log(result);
+          this.myform.reset();
+        //   this.dialog.open(successmodal, {
+        // data: {
+        //   data: 'Successfully submitted'
+        //       }
+        //     });
+        }
+      })
     }
 
   }
